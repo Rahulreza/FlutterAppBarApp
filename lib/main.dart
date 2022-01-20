f927b940a896912d0258fb68f48f63c68edd2955
Rahulreza <rahulreza001@gmail.com>
2022-01-20 17:17:07 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+void main(){
+ runApp(new MaterialApp(
+   home: MyApp(),
+ ));
+}
+class MyApp extends StatefulWidget{
+  @override
+  _State createState()=> _State();
+
+}
+class _State extends State<MyApp>{
+  int _value=0;
+  void _add()=>setState(()=>_value++);
+  void _remove()=>setState(()=>_value--);
+ @override
+  Widget build(BuildContext context) {
+
+ return new Scaffold(
+   appBar: AppBar(
+     title: Text("Basic Structure of  AppBar"),
+     backgroundColor: Colors.red,
+     actions: [
+       IconButton(icon: Icon( Icons.add),onPressed: _add ),
+       IconButton(icon: Icon( Icons.remove),onPressed: _remove ),
+     ],
+   ),
+   body: Container(
+     padding: EdgeInsets.all(32.0),
+     child: Center(
+       child: Column(
+         children: [
+           Text(_value.toString(),style: TextStyle(fontWeight:FontWeight.bold ,fontSize: 37.0),),
+         ],
+       ),
+     ),
+   ),
+ );
+  }
+}
